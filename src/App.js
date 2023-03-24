ce64dbcd556f1dc4e25e7a16bd5b348d32266c30
Nirmalraj <nirmalraj.joseph@twilightsoftwares.com>
2023-03-24 15:27:31 +0530

    main commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header'
-import Site from './components/site/site';
+//  import Header from './components/header/Header'
+ import Site from './components/site/site';
 function App() {
   return (
     <div className="App">
-    <Site />
-       <Header /> 
+     <Site /> 
+        {/* <Header />   */}
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,76 @@
-import React from 'react'
-import './header.css';
+import React from "react";
+import "./header.css";
+
 const header = () => {
   return (
     <div>
- <header className='header'>
-        <nav className='nav container'>
-        <a href='index.html' className='nav__logo'>Nirmal</a>
+      <header className="header">
+        <nav className="nav container">
+          <a href="index.html" className="nav__logo">
+            Nirmal
+          </a>
 
-<div className='nav__menu'>
-<ul li className='nav__list grid' >
-    <li li className='nav__item' >
-    
-    {/* <a>Home
-    <i className='uil uil- nav__icon'></i>
-    </a> */}
-    
-    </li>
-</ul>
+          <div className="nav__menu">
+            <ul li className="nav__list grid">
+              <li li className="nav__item">
+                <a href="#Home" className="nav__link" active-link>
+                Home
+                <i className="uil uil-estate nav__icon"></i>
+                </a>
+              </li>
+              <li li className="nav__item">
+                <a href="#About" className="nav__link">
 
-</div>
+                  
+                  <i className="uil uil-user nav__icon"></i>
+                  About
+                </a>
+              </li>
+              <li li className="nav__item">
+                <a href="#Skills" className="nav__link">
+
+                  <i className="uil uil-fil-alt nav__icon"></i>
+                  Skills
+
+                </a>
+              </li>
+              <li li className="nav__item">
+                <a href="#services" className="nav__link">
+
+                  <i className="uil uil-briefcase nav__icon"></i>
+                  Services
+
+                </a>
+              </li>
+              <li li className="nav__item">
+                <a href="#portfolio" className="nav__link">
+
+                  <i className="uil uil-scenery nav__icon"></i>
+                  Portfolio
+
+                </a>
+              </li>
+              <li li className="nav__item">
+                <a href="#contact" className="nav__link">
+
+                  <i className="uil uil-message nav__icon"></i>
+                  Contact
+
+                </a>
+              </li>
+
+
+            </ul>
+
+            <i class="uil uil-times nav__close"></i>
+          </div>
+          <div className="nav__toggle">
+            <i class="uil uil-apps"></i>
+          </div>
         </nav>
-    </header>
+      </header>
     </div>
-   
-  )
-}
+  );
+};
 
-export default header
+export default header;
