61e5467eeb5e7decf82bfdedf28c27bca621b9a2
Nirmalraj <nirmalraj.joseph@twilightsoftwares.com>
2023-03-27 17:03:03 +0530

    Added Work component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import "./header.css";
 
-const Header = () => { /* Change Background Header*/
+const Header = () => { 
+    /* Change Background Header*/
     window.addEventListener('scroll', function () {
         const header = document.querySelector(".header");
         if (this.scrollY >= 800) 
--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,9 +1,9 @@
 
-import Work1 from "../../assets/Work1"
-import Work2 from "../../assets/Work2"
-import Work3 from "../../assets/Work3"
-import Work4 from "../../assets/Work4"
-import Work5 from "../../assets/Work5"
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
     {
@@ -44,7 +44,7 @@ export const projectsData = [
 
     },
     {
-        name:'web',
+        name:'Web',
 
     },
     {
